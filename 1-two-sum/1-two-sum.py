b955e85e3a5ae8432c4a0d03a7ae62e873a9b1aa
Ashwin <83164687+AshWinShaRma37@users.noreply.github.com>
2022-07-12 14:37:52 +0530

    Update 1-two-sum.py

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic = {}
+        dic = {} #create ductionary wto store num in nums with their index
         for i, num in enumerate(nums):
+            #find target - num in dic along with creating dictionary with num in nums with their index
             if target - num in dic:
                 return dic[target-num], i
             dic[num] = i
