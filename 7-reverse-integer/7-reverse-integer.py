6a6c924f29ec7138d5432aa6c65e1f76a848e69b
Ashwin <83164687+AshWinShaRma37@users.noreply.github.com>
2022-07-13 18:08:56 +0530

    Update 7-reverse-integer.py

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -4,7 +4,7 @@ class Solution:
         if x >= 2**31-1 or x <= -2**31: return 0
         else:
             if x>0:
-                a=int(str(x)[::-1])
+                a=int(str(x)[::-1]) #converting into string to inverse it and then again converting to int to remove the 0's that might come in the beginning
             if x<0:
                 a=-(int(str(-x)[::-1]))
         if a >= 2**31-1 or a <= -2**31: return 0
