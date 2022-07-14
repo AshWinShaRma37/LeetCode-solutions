c94fb0c4ffd03d2850b8d1b5c683ac1a7faea58f
Ashwin <83164687+AshWinShaRma37@users.noreply.github.com>
2022-07-14 11:06:15 +0530

    Time: 47 ms (69.33%), Space: 13.9 MB (79.48%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,13 +1,21 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.lstrip()
+        #removing whitespaces in front
+        #this is to make sure other whitespaces are taken into consideration
+        s = s.lstrip() 
+        #creating an empty string to append the digits
         g = ""
-        if g==s: return 0
+        #if s is empty no need for checking other things they may not work as well 
+        if g==s: return 0  
+        
+        #checking the sign
         if s[0]=="-":
             g="-"
             s = s[1:]
         elif s[0]=="+":
             s = s[1:]
+            
+        #checking and appending digits to string
         for a in s:
             try:
                 int(a)
@@ -16,7 +24,11 @@ class Solution:
             except:
                 break
         if g.replace("-","")=="": g="0"
+            
+        #converting the resultant string to int
         n =int(g)
+        
+        #making sure the integer is in range[-2**31,2**31-1]
         if n<=-pow(2,31): return -pow(2,31)
         elif n>=pow(2,31): return pow(2,31) - 1
         else: return n
