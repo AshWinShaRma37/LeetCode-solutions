87d4678991231b8cdae7e920c7ab5c6074c15387
Ashwin <83164687+AshWinShaRma37@users.noreply.github.com>
2022-07-14 17:24:22 +0530

    Time: 58 ms (79.65%), Space: 13.9 MB (30.61%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,18 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        #creating a dictionary to store values of each symbol
         dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        i=0
-        rom=0
+        i=0 #index
+        rom=0  #by default roman number set to 0
         while (i < len(s)):
+            #getting the value of ith character in string
             s1 = dic[(s[i])]
             if (i + 1 < len(s)):
+                #getting value of next element
                 s2 = dic[(s[i + 1])]
+                #checking based on next element if it is to be added or subtracted
                 if (s1 >= s2):
                     rom = rom + s1
                     i = i + 1
                 else:
                     rom = rom + s2 - s1
                     i = i + 2
+            #if no more elements left simply add the value
             else:
                 rom = rom + s1
                 i = i + 1
