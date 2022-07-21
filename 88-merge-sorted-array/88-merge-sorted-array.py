26c5f7ab2085f1e200790498d027482203cefaae
Ashwin <83164687+AshWinShaRma37@users.noreply.github.com>
2022-07-21 16:41:53 +0530

    Time: 80 ms (8.97%), Space: 13.8 MB (85.55%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,4 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        #can't sure sort() function here 
+        #as it returns nothing and makes changes to the original sequence
         nums1[:] = sorted(nums1[:m]+nums2[:n])
