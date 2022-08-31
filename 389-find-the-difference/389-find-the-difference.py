abae65733c94240780097bdc25ba7060344313a6
Ashwin Sharma <83164687+AshWinShaRma37@users.noreply.github.com>
2022-08-31 12:06:58 +0530

    Time: 43 ms (80.03%), Space: 13.9 MB (31.85%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,8 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        #concating both the string and checking if any character counts is odd
-        for a in (s+t):
-            if (s+t).count(a)%2==1:
+        st = s+t
+        #concating the strings and checking if any character has a odd count
+        for a in st:
+            if st.count(a)%2==1:
                 return a
         
             
