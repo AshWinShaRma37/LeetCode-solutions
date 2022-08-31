719bfb67553d905e11b8de2be85809fcdbd593e6
Ashwin Sharma <83164687+AshWinShaRma37@users.noreply.github.com>
2022-08-31 12:11:58 +0530

    Time: 53 ms (33.97%), Space: 13.8 MB (95.94%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,6 +1,9 @@
 class Solution:
     def interpret(self, command: str) -> str:
+        '''
         command = command.replace("()","o")
         command = command.replace("(al)","al")
         return command
-        
+        '''
+        #the same could be done in one line
+        return command.replace('()','o').replace('(al)','al')
